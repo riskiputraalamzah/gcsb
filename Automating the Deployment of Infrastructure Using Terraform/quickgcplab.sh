58df76941fc29a5d8502b64a902bb1d6142d0e16
Riski Putra Alamzah <71111236+riskiputraalamzah@users.noreply.github.com>
2024-10-08 00:10:01 +0700

    Update quickgcplab.sh

--- Automating the Deployment of Infrastructure Using Terraform/quickgcplab.sh
+++ Automating the Deployment of Infrastructure Using Terraform/quickgcplab.sh
@@ -30,7 +30,7 @@ cd tfinfra
 
 wget https://raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Automating%20the%20Deployment%20of%20Infrastructure%20Using%20Terraform/provider.tf
 
-https://raw.githubusercontent.com/riskiputraalamzah/gcsb/refs/heads/main/Automating%20the%20Deployment%20of%20Infrastructure%20Using%20Terraform/terraform.tfstate
+wget https://raw.githubusercontent.com/riskiputraalamzah/gcsb/refs/heads/main/Automating%20the%20Deployment%20of%20Infrastructure%20Using%20Terraform/terraform.tfstatewget 
 
 wget https://raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Automating%20the%20Deployment%20of%20Infrastructure%20Using%20Terraform/variables.tf
 
